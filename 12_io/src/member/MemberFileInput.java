fe36a5ba042b7bb209009c94bcb90ed69981925d
mintcoffeee <you6735@naver.com>
2023-02-18 20:49:17 +0900

    java_13day

--- 12_io/src/member/MemberFileInput.java
+++ 12_io/src/member/MemberFileInput.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class MemberFileInput implements Member {
 
 	@Override
-	public void execute(List<MemberDTO> list) throws IOException, ClassNotFoundException {
+	public void execute(List<MemberDTO> list) throws IOException, ClassNotFoundException{
 	    ObjectInputStream ois = new ObjectInputStream(new FileInputStream("member.txt"));
 	    list.clear();	//리스트의값 제거 
 	    try {
@@ -19,7 +19,14 @@ public class MemberFileInput implements Member {
 	        }
 	    } catch (EOFException e) {
 	        // End of file reached, do nothing
-	    } finally {
+	    	
+	    	// 연쇄반응으로 다른 class 의 override 함수를 수정 하지 않기 위해서 try catch 사용을 하는 것이 좋다.
+//	    } catch (ClassNotFoundException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+	    	
+		} finally {
 	        ois.close();
 	        System.out.println("파일 읽기 완료");
 	    }
